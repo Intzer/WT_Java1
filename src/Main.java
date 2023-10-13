50871bd69353fc267e51231330f574edd7cc75be
Roman <rmilh1@mail.ru>
2023-10-13 11:55:11 +0300

    Вынести логику в отдельный класс

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,11 @@ public class Main {
         Scanner in = new Scanner(System.in);
 
         System.out.print("Input x: ");
-        float x = in.nextFloat();
+        double x = in.nextFloat();
         System.out.print("Input y: ");
-        float y = in.nextFloat();
+        double y = in.nextFloat();
 
-        double up = 1 + Math.pow(Math.sin(x + y), 2.0);
-        double down = 2 + Math.abs(x - (2*x)/(1+Math.pow(x, 2.0)*Math.pow(y, 2.0)));
-        double res = (up / down) + x;
-
-        System.out.println("Result: " + res);
+        Resolver resolver = new Resolver(x, y);
+        System.out.println("Result: " + resolver.Solve());
     }
 }
--- src/Resolver.java
+++ src/Resolver.java
@@ -1,2 +1,16 @@
 public class Resolver {
+    private double x;
+    private double y;
+
+    public Resolver(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double Solve() {
+        double up = 1 + Math.pow(Math.sin(x + y), 2.0);
+        double down = 2 + Math.abs(x - (2*x)/(1+Math.pow(x, 2.0)*Math.pow(y, 2.0)));
+        double res = (up / down) + x;
+        return res;
+    }
 }
